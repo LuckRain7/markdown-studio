037adb945d3f5d1890aace372844d9a4efd9560b
Ra1n <luckrain7@foxmail.com>
2026-06-09 10:32:35 +0800

    chore: 配置 vite base 以适配自定义域名部署

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  // 部署在自定义域名 http://md.ilabubu.com/ 根路径下
+  base: '/',
   plugins: [vue()],
   server: {
     port: 5173,
